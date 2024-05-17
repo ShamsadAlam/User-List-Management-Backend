c41682b61db333c3b644177cfbcb0b7cd0a66901
ShamsadAlam <nobleperson7571@gmail.com>
2024-05-17 23:58:39 +0530

    cloud mongo updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/userlist");
+mongoose.connect(`${process.env.MONGODB_URI}`);
 
 const listRoutes = require("./routes/listRoutes");
 const userRoutes = require("./routes/userRoutes");
